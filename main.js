04fd06daa69d182019e217759d6757130a54e7ed
StevieinWaterford <stevieindub@gmail.com>
2021-08-29 14:19:28 +0000

    Add rearangement of code

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
-let trueAnswers = ['b', 'a', 'a', 'b'];
+let trueAnswers = ['b', 'a', 'a', 'd'];
 let quizForm = document.querySelector('.quiz');
 let result = document.querySelector('.results-area');
+let displayPercentage = document.querySelector('.displayPercentage');
+
 
 
 quizForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
     let percentage = 0;
-    let selectedAnswers = [quizForm.q1.value, quizForm.q2.value, quizform.q3.value, quizform.q4.value]
+    let selectedAnswers = [quizForm.q1.value, quizForm.q2.value, quizForm.q3.value, quizForm.q4.value]
 
     selectedAnswers.forEach((answer, index) => {
         if (answer === trueAnswers[index])
-            percentage += 25
+            percentage += 25;
 
     })
+
 })
 
-let displayPercentage = 0;
 
-let timer = setInterval(() => {
-    result.querySelector('span').textContent = `${displayPercentage}`;
-    if (displayPercentage === percentage) {
-        clearInterval(timer);
-    } else {
-        displayPercentage++
-    }
-})
+displayPercentage.textContent = percentage;
+
+
+
+
+
+//let timer = setInterval(() => {
+    //result.querySelector('span').textContent = `${displayPercentage}`;
+    //if (displayPercentage === percentage) {
+        //clearInterval(timer);
+   // } else {
+        //displayPercentage++
+   // }
+//}, 20);
 
 
